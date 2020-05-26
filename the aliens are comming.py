b258bcb9e7b44055a88ae6855f207176fb590085
Vallas25 <65953833+Vallas25@users.noreply.github.com>
2020-05-26 16:31:34 +0200

    Update the aliens are comming.py

--- the aliens are comming.py
+++ the aliens are comming.py
@@ -9,20 +9,23 @@ b = Bridge(bridge_ip)
 
 b.connect()
 
-b.get_light("lamp 1", "on")
-b.get_light("lamp 2", "on")
+lamp_1 = "name of your lamp"
+lamp_2 = "name of your other lamp"
+
+b.get_light(lamp_1, "on")
+b.get_light(lamp_2, "on")
 
 while True:
     sleep_time_livingroom = random.randint(1, 3)
     brightness_livingroom = random.randint(1, 254)
     sleep_time_kitchen = random.randint(1, 3)
     brightness_kitchen = random.randint(1, 254)
-    b.set_light("lamp 1", "on", True)
-    b.set_light("lamp 1", "bri", brightness_livingroom)
+    b.set_light(lamp_1, "on", True)
+    b.set_light(lamp_1, "bri", brightness_livingroom)
     time.sleep(sleep_time_livingroom)
-    b.set_light("lamp 1", "on", False)
+    b.set_light(lamp_1, "on", False)
 
-    b.set_light("lamp 2", "on", True)
-    b.set_light("lamp 2", "bri", brightness_kitchen)
+    b.set_light(lamp_2, "on", True)
+    b.set_light(lamp_2, "bri", brightness_kitchen)
     time.sleep(sleep_time_kitchen)
-    b.set_light("lamp 2", "on", False)
+    b.set_light(lamp_2, "on", False)
